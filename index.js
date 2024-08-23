4895ddcd198752daa051ffd948ac9e1853d442d1
Claudio <claudio.marino@outlook.com>
2024-08-23 09:26:25 +0200

    saved the darkMode state in localStorage

--- index.js
+++ index.js
@@ -6,7 +6,25 @@ const $iEl = $('i-el')
 
 $heart.addEventListener('click', darkMode)
 
+// Leer estado darkMode desde el localStorage
+let isDarkMode = JSON.parse(localStorage.getItem('darkMode'))
+
+// Estado inicial del darkMode
+if (isDarkMode) {
+	$bodyEl.classList.add('darkMode')
+	$iEl.classList.add('darkMode')
+}
+
 function darkMode() {
-	$bodyEl.classList.toggle('darkMode')
-	$iEl.classList.toggle('darkMode')
+	if (isDarkMode) {
+		$bodyEl.classList.remove('darkMode')
+		$iEl.classList.remove('darkMode')
+	} else {
+		$bodyEl.classList.add('darkMode')
+		$iEl.classList.add('darkMode')
+	}
+
+	// Cambiar el estado y guardarlo en localStorage
+	isDarkMode = !isDarkMode
+	localStorage.setItem('darkMode', JSON.stringify(isDarkMode))
 }
